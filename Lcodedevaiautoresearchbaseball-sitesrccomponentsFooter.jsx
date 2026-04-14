c5015a3305befdf602592d09decaaeee0ddab636
= <=>
2026-04-14 06:18:33 -0500

    Mark deleted files for removal

--- Lcodedevaiautoresearchbaseball-sitesrccomponentsFooter.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; 2026 Baseball Info. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- Lcodedevaiautoresearchbaseball-sitesrcdatahofInductees.js
+++
@@ -1,5 +0,0 @@
-export default [
-  { name: "Babe Ruth", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { name: "Jackie Robinson", description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-  { name: "Willie Mays", description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium." }
-];
